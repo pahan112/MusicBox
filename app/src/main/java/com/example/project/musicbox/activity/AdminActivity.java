1b1e8582ccde3bfedc7125bed7ea0153bbbf8173
Pahan112 <kinglich112@gmail.com>
2017-07-15 17:01:03 +0300

    start

--- app/src/main/java/com/example/project/musicbox/activity/AdminActivity.java
+++ app/src/main/java/com/example/project/musicbox/activity/AdminActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.example.project.musicbox.R;
+
 /**
  * Created by Pahan on 15.07.2017.
  */
@@ -12,5 +14,6 @@ public class AdminActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
     }
 }
